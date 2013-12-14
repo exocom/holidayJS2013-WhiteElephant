1c34b1ace668754e22275cabab1f3a2b67a767eb
Nate Fuller <fuller.nate@gmail.com>
2013-12-14 16:01:03 -0700

    Add Firebase

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,7 +4,8 @@ angular.module('holidayJs2013WhiteElephantApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ngRoute'
+  'ngRoute',
+  'firebase'
 ])
   .config(['$routeProvider', function ($routeProvider) {
     $routeProvider
