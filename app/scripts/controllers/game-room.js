e81f18e261a64da782f0bd457c8afbde9d22dc9a
Ed Gilmore <edgilmore@live.com>
2013-12-14 15:40:40 -0700

    Intial Commit

---
+++ app/scripts/controllers/game-room.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('holidayJs2013WhiteElephantApp')
+  .controller('GameRoomCtrl', ['$scope', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  }]);
