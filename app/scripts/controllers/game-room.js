2b092339d92fd3c516c34eff670585100d04f5db
Nate Fuller <fuller.nate@gmail.com>
2013-12-14 17:27:42 -0700

    Integrate Firebase ref

--- app/scripts/controllers/game-room.js
+++ app/scripts/controllers/game-room.js
@@ -1,27 +1,26 @@
 'use strict';
 
 angular.module('holidayJs2013WhiteElephantApp')
-	.controller('GameRoomCtrl', ['$scope', function ($scope) {
-		console.log('here');
-		$scope.awesomeThings = [
-			'HTML5 Boilerplate',
-			'AngularJS',
-			'Karma'
-		];
-		$scope.users = [];
-		$scope.presents = [];
+	.controller('GameRoomCtrl', ['$scope', '$firebase', function ($scope, $firebase) {
+		console.log('GameRoom');
+
+        var ref = new Firebase("https://hack-holiday-js.firebaseio.com/");
+        var base = $firebase(ref);
+
+		$scope.users = base.$child("users");
+		$scope.presents = base.$child("presents");
 		$scope.activeUsersId = null;
 
 		$scope.addUserWithPresent = function (name) {
 			var id = Math.floor(Math.random() * 100 + 1);
 			var presentId = Math.floor(Math.random() * 100 + 1);
-			$scope.presents.push({
+			$scope.presents.$add({
 				id: presentId,
 				userId: id,
-				url: ''
+				url: 'http://lorempixel.com/64/64/technics/'
 			});
 
-			$scope.users.push({
+			$scope.users.$add({
 				id: id,
 				name: name,
 				currentPresentsId: null,
