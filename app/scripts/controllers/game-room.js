71691b9eeb3946e628d96ca47b977c65569aaa61
Ed Gilmore <edgilmore@live.com>
2013-12-14 17:10:31 -0700

    added user and present logic to game-room controller

--- app/scripts/controllers/game-room.js
+++ app/scripts/controllers/game-room.js
@@ -1,10 +1,31 @@
 'use strict';
 
 angular.module('holidayJs2013WhiteElephantApp')
-  .controller('GameRoomCtrl', ['$scope', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  }]);
+	.controller('GameRoomCtrl', ['$scope', function ($scope) {
+		console.log('here');
+		$scope.awesomeThings = [
+			'HTML5 Boilerplate',
+			'AngularJS',
+			'Karma'
+		];
+		$scope.users = [];
+		$scope.presents = [];
+		$scope.activeUsersId = null;
+
+		$scope.addUserWithPresent = function (name) {
+			var id = Math.floor(Math.random() * 100 + 1);
+			var presentId = Math.floor(Math.random() * 100 + 1);
+			$scope.presents.push({
+				id: presentId,
+				userId: id,
+				url: ''
+			});
+
+			$scope.users.push({
+				id: id,
+				name: name,
+				currentPresentsId: null,
+				presentId: presentId
+			});
+		};
+	}]);
